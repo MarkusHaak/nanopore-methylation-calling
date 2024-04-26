afb6924cd4027ab28794b7e71bf26d003e0732d2
Markus Haak <markus.haak@posteo.de>
2024-04-26 18:10:30 +0200

    Fix minor bug, change output to total_runtime.txt

--- workflow/scripts/contigcov_from_genomecov.py
+++ workflow/scripts/contigcov_from_genomecov.py
@@ -4,6 +4,6 @@ df = pd.read_csv(f"{snakemake.input}", sep="\t", header=None, names=['contig', '
 d = df.groupby('contig').agg(['sum','max'])
 con = pd.DataFrame(d[('cov', 'sum')] / d[('pos', 'max')], columns=['depth'])
 con['size'] = d[('pos', 'max')]
-for i in config['contig_coverages']:
+for i in snakemake.config['contig_coverages']:
     con[f'min_{i}'] = df.groupby('contig')['cov'].apply(lambda c: (c >= i).sum() / len(c) * 100.)
 con.to_csv(f"{snakemake.output}", float_format='%.2f', sep='\t')
