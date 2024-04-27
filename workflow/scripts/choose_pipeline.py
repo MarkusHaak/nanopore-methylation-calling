a4486713c824f38859f5cfd95eb0d3f3da9d5ba8
Markus Haak <markus.haak@posteo.de>
2024-04-27 02:06:48 +0200

    Assume r10.4.1 if pore is r10.4

--- workflow/scripts/choose_pipeline.py
+++ workflow/scripts/choose_pipeline.py
@@ -8,6 +8,9 @@ with open(f"{snakemake.output[0]}", 'w') as f:
     print(config_name, file=f)
 
 pore = config_name_to_pore(config_name)
+if pore == "r10.4":
+    print("WARNING: assuming pore r10.4.1 on the basis of pore r10.4")
+    pore = "r10.4.1"
 print("pore:", pore)
 with open(f"{snakemake.output[1]}", 'w') as f:
     print(pore, file=f)
